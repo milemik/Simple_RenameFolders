a3b48a1f3486dc1ef01f9e0b463e1be60132a53d
Ivan Miletic <39780627+milemik@users.noreply.github.com>
2018-06-28 01:42:32 +0200

    Update rename.py

--- rename.py
+++ rename.py
@@ -3,15 +3,17 @@
 import os
 import random
 from glob import glob
-
+#get the path to curent working dir
 pwd = os.getcwd()
-
+#find all sub dir in that path
 list_dir = glob('{}/*/'.format(pwd))
-
+# For every dir path in list_dir
 for x in list_dir:
+    #give random number 0 - 100
     num = random.randint(0,100)
+    #rename the dir to Folder+randnum EXAMPLE: Folder66
     os.rename(x,'{}/Folder{}'.format(pwd,num))
-
+#after rename all folders print "Rename COMPLITED"
 print("Rename COMPLITED!")
 
 
